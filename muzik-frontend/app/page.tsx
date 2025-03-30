3aa1bcddebf349151debc167cd4f668a29688e58
Vatsal-Patel-09 <vatsalpatel952005@gmail.com>
2025-03-30 17:01:51 +0530

    aded about testimonials tutor team etc

--- muzik-frontend/app/page.tsx
+++ muzik-frontend/app/page.tsx
@@ -1,13 +1,16 @@
-import About from "@/components/About";
+
+import { About } from "@/components/About";
 import Contact from "@/components/Contact";
 import Courses from "@/components/Courses";
 import Faq from "@/components/Faq";
 import Footer from "@/components/Footer";
-import Hero from "@/components/Hero";
+import { Hero } from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import Services from "@/components/Services";
-import Testimonials from "@/components/Testimonials";
-import TutorTeam from "@/components/Tutor-Team";
+import { Testimonials } from "@/components/Testimonials";
+
+import { TutorTeam } from "@/components/Tutor-Team";
+
 import Image from "next/image";
 
 export default function Home() {
