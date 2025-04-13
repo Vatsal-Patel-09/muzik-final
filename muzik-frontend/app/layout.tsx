80c311145320ff964dd6b03ed6fb34dd12d88849
Vatsal-Patel-09 <vatsalpatel952005@gmail.com>
2025-04-13 23:02:11 +0530

    meta done

--- muzik-frontend/app/layout.tsx
+++ muzik-frontend/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Head from "next/head";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,6 +15,8 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+
 
 export const metadata: Metadata = {
   title: "Muzik Skill House",
@@ -31,33 +34,36 @@ export default function RootLayout({
   return (
 
     <html lang="en">
-      <Head>
-        {/* Facebook Pixel Base Code */}
-        <script
+      <>
+        <Script
+          id="fb-pixel-script"
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
               !function(f,b,e,v,n,t,s)
               {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
               n.callMethod.apply(n,arguments):n.queue.push(arguments)};
               if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
+              n.queue=[];
+              t=b.createElement(e);t.async=!0;
               t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              s.parentNode.insertBefore(t,s)}(window, document, 'script',
               'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '516326594884066'); 
+              fbq('init', '991488343116364');
               fbq('track', 'PageView');
             `,
           }}
         />
         <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: 'none' }}
-            src={`https://www.facebook.com/tr?id=516326594884066&ev=PageView&noscript=1`}
-          />
-        </noscript>
-      </Head>
+        <img
+          height="1"
+          width="1"
+          style={{ display: 'none' }}
+          src="https://www.facebook.com/tr?id=991488343116364&ev=PageView&noscript=1"
+          alt="facebook pixel"
+        />
+      </noscript>        
+      </>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
